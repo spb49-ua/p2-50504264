f598bb4118fb1e2e2b9b26eb83ff0cc1e30f26a6
spb49-ua <98749056+spb49-ua@users.noreply.github.com>
2022-02-10 19:43:09 +0100

    create Hero y create Enemy

--- game/prac1.cc
+++ game/prac1.cc
@@ -9,6 +9,16 @@ const int KNAME=32;
 const int KENEMIES=5;
 const int KPOINTS=200;
 const int KDICE=20; // Número de caras del dado
+const int AXOLOTL_ATTACK=40;
+const int AXOLOTL_DEFENSE=40;
+const int TROLL_ATTACK=60;
+const int TROLL_DEFENSE=80;
+const int ORC_ATTACK=80;
+const int ORC_DEFENSE=120;
+const int HELLHOUND_ATTACK=120;
+const int HELLHOUND_DEFENSE=100;
+const int DRAGON_ATTACK=160;
+const int DRAGON_DEFENSE=140;
 
 struct Core{
   int attack;
@@ -42,13 +52,12 @@ int rollDice(){
   return rand()%KDICE+1;
 }
 
-Hero nombreHero(){
-  Hero player;
+Hero nombreHero(Hero player){
   int n;
   bool name_error;
   do{
   name_error=false;
-  cout<<"Enter hero name:\t";
+  cout<<"Enter hero name: ";
   cin.getline(player.name,KNAME-1);
   n=strlen(player.name);
   if(isalpha(player.name[0])==0)
@@ -64,14 +73,13 @@ Hero nombreHero(){
   return player;
 }
 
-Hero attack_defense(){
-  Hero player;
+Hero attack_defense(Hero player){
   bool error;
   int n1,n2;
   char c;
   do{
   error=false;
-  cout<<"Enter attack/defense:\t";
+  cout<<"Enter attack/defense: ";
   cin>>n1>>c>>n2;
   if(n1<=0){
     cout<<"ERROR: wrong distribution"<<endl;
@@ -88,19 +96,102 @@ Hero attack_defense(){
   }while(error);
   player.features.attack=n1*KPOINTS/100;
   player.features.defense=n2*KPOINTS/100;
-  player.features.hp=n2*player.features.defense;
+  player.features.hp=2*player.features.defense;
+  
   return player;
 }
 
-Hero createHero(){
-  Hero player;
-  player=nombreHero();
-  player=attack_defense();
+Hero createHero(Hero player){
+  player=nombreHero(player);
+  player=attack_defense(player);
   return player;
 }
 
-/*Enemy createEnemy(){
-}*/
+Enemy selectEnemy(Enemy enemigo){
+  int n;
+  n=rollDice();
+  switch(n){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      enemigo.name=AXOLOTL;
+    break;
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+      enemigo.name=TROLL;
+    break;
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+      enemigo.name=ORC;
+    break;
+    case 16:
+    case 17:
+    case 18:
+      enemigo.name=HELLHOUND;
+    break;
+    case 19:
+    case 20:
+      enemigo.name=DRAGON;
+    break;
+  }
+  return enemigo;
+}
+
+Enemy enemyStats(Enemy enemigo){
+  cout<<"[Enemy]"<<endl;
+  cout<<"Breed: ";
+  switch(enemigo.name){
+    case 0:
+      cout<<"Axolotl";
+      enemigo.features.attack=AXOLOTL_ATTACK;
+      enemigo.features.defense=AXOLOTL_DEFENSE;
+      enemigo.features.hp=2*AXOLOTL_DEFENSE;
+    break;
+    case 1:
+      cout<<"Troll";
+      enemigo.features.attack=TROLL_ATTACK;
+      enemigo.features.defense=TROLL_DEFENSE;
+      enemigo.features.hp=2*TROLL_DEFENSE;
+    break;
+    case 2:
+      cout<<"Orc";
+      enemigo.features.attack=ORC_ATTACK;
+      enemigo.features.defense=ORC_DEFENSE;
+      enemigo.features.hp=2*ORC_DEFENSE;
+    break;
+    case 3:
+      cout<<"Heallhound";
+      enemigo.features.attack=HELLHOUND_ATTACK;
+      enemigo.features.defense=HELLHOUND_DEFENSE;
+      enemigo.features.hp=2*HELLHOUND_DEFENSE;
+    break;
+    case 4:
+        cout<<"Dragon";
+      enemigo.features.attack=DRAGON_ATTACK;
+      enemigo.features.defense=DRAGON_DEFENSE;
+      enemigo.features.	hp=2*DRAGON_DEFENSE;
+    break;
+    }
+    cout<<endl;
+    cout<<"Attack: "<<enemigo.features.attack<<endl;
+    cout<<"Defense: "<<enemigo.features.defense<<endl;
+    cout<<"Health points: "<<enemigo.features.hp<<endl;
+    return enemigo;
+}
+
+Enemy createEnemy(Enemy enemigo){
+  enemigo=selectEnemy(enemigo);
+  enemigo=enemyStats(enemigo);
+  return enemigo;
+}
 
 void fight(Hero &hero,Enemy &enemy){
 }
@@ -118,8 +209,36 @@ void showMenu(){
        << "Option: ";
 }
 
+void fight(){
+}
+
+void menu(){
+char op='q';
+do{
+  if(op!='1'&&op!='2'&&op!='3'&&op!='4'&&op!='q')
+    cout<<"ERROR: wrong option"<<endl;
+  showMenu();
+  cin>>op;
+  }while(op!='1'&&op!='2'&&op!='3'&&op!='4'&&op!='q');
+  
+  switch(op){
+    case '1':
+    fight();
+    break;
+    case '2':
+    break;
+    case '3':
+    break;
+    case '4':
+    break;
+    case 'q':
+    break;
+  }
+}
+
 int main(int argc,char *argv[]){
-  createHero();
+  Hero player;
+  Enemy enemigo;
   if(argc!=2){ // Si los parámetros no son correctos, el programa termina inmediatamente
     cout << "Usage: " << argv[0] << " <seed>" << endl;
   }
@@ -127,5 +246,14 @@ int main(int argc,char *argv[]){
     srand(atoi(argv[1])); // Introducimos la semilla para generar números aleatorios
     
     // Aquí vendrá todo tu código del "main"...
+    
+  player=createHero(player);
+  enemigo=createEnemy(enemigo);
+  menu();
+  
+  
+    
+    
   }
 }
+
