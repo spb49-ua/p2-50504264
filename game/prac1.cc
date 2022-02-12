64e3a2d839b5a18f4a0ed2f168174ee2f252288c
spb49-ua <98749056+spb49-ua@users.noreply.github.com>
2022-02-12 16:09:27 +0100

    options finished

--- game/prac1.cc
+++ game/prac1.cc
@@ -19,6 +19,11 @@ const int HELLHOUND_ATTACK=120;
 const int HELLHOUND_DEFENSE=100;
 const int DRAGON_ATTACK=160;
 const int DRAGON_DEFENSE=140;
+const int AXOLOTL_EXP=100;
+const int TROLL_EXP=150;
+const int ORC_EXP=200;
+const int HELLHOUND_EXP=300;
+const int DRAGON_EXP=400;
 
 struct Core{
   int attack;
@@ -104,6 +109,12 @@ Hero attack_defense(Hero player){
 Hero createHero(Hero player){
   player=nombreHero(player);
   player=attack_defense(player);
+  player.special=true;
+  player.runaways=3;
+  player.exp=0;
+  for(int i =0;i<KENEMIES;i++){
+    player.kills[i]=0;
+  }
   return player;
 }
 
@@ -168,7 +179,7 @@ Enemy enemyStats(Enemy enemigo){
       enemigo.features.hp=2*ORC_DEFENSE;
     break;
     case 3:
-      cout<<"Heallhound";
+      cout<<"Hellhound";
       enemigo.features.attack=HELLHOUND_ATTACK;
       enemigo.features.defense=HELLHOUND_DEFENSE;
       enemigo.features.hp=2*HELLHOUND_DEFENSE;
@@ -193,10 +204,135 @@ Enemy createEnemy(Enemy enemigo){
   return enemigo;
 }
 
-void fight(Hero &hero,Enemy &enemy){
+void enemyKilled(Hero &player,Enemy &enemigo){
+    player.kills[enemigo.name]++;
+    switch(enemigo.name){
+      case 0:
+      player.exp=player.exp + AXOLOTL_EXP;
+      break;
+      case 1:
+      player.exp=player.exp + TROLL_EXP;
+      break;
+      case 2:
+      player.exp=player.exp + ORC_EXP;
+      break;
+      case 3:
+      player.exp=player.exp + HELLHOUND_EXP;
+      break;
+      case 4:
+      player.exp=player.exp + DRAGON_EXP;
+      break;
+    }
 }
 
-void report(const Hero &hero){
+void heroToEnemy(Hero &player,Enemy &enemigo,int extra){
+  int n1,n2;
+  n1=rollDice()*5*extra;
+  n2=rollDice()*5;
+  cout<<"[Hero -> Enemy]"<<endl;
+  cout<<"Attack: "<<player.features.attack<<" + "<<n1<<endl;
+  cout<<"Defense: "<<enemigo.features.defense<<" + "<<n2<<endl;
+  cout<<"Hit points: ";
+  if((player.features.attack+n1-(enemigo.features.defense+n2))>0){
+    cout<<player.features.attack+n1-(enemigo.features.defense+n2);
+    enemigo.features.hp=enemigo.features.hp-(player.features.attack+n1-(enemigo.features.defense+n2));
+    }
+  else
+  	cout<<"0";
+  cout<<endl;
+  cout<<"Enemy health points: ";
+  if(enemigo.features.hp<0)
+    enemigo.features.hp=0;
+  cout<<enemigo.features.hp<<endl;
+  if(enemigo.features.hp==0){
+    cout<<"Enemy killed"<<endl;
+    enemyKilled(player,enemigo);
+  }
+}
+
+void enemyToHero(Hero &player,Enemy &enemigo){
+  int n1,n2;
+  n1=rollDice()*5;
+  n2=rollDice()*5;
+  cout<<"[Enemy -> Hero]"<<endl;
+  cout<<"Attack: "<<enemigo.features.attack<<" + "<<n1<<endl;
+  cout<<"Defense: "<<player.features.defense<<" + "<<n2<<endl;
+  cout<<"Hit points: ";
+  if((enemigo.features.attack+n1-(player.features.defense+n2))>0){
+    cout<<enemigo.features.attack+n1-(player.features.defense+n2);
+    player.features.hp=player.features.hp-(enemigo.features.attack+n1-(player.features.defense+n2));
+    }
+  else
+  	cout<<"0";
+  cout<<endl;
+  cout<<"Hero health points: ";
+  if(player.features.hp<0)
+    player.features.hp=0;
+  cout<<player.features.hp<<endl;
+}
+
+void fight(Hero &player,Enemy &enemigo,bool &runawayAvailable){
+  runawayAvailable=true;
+  heroToEnemy(player,enemigo,1);
+  if(enemigo.features.hp>0)
+    enemyToHero(player,enemigo);
+  else{
+    enemigo=createEnemy(enemigo);
+  }
+}
+
+void runaway(Hero &player,Enemy &enemigo,bool &runawayAvailable){
+  if(player.runaways==0||runawayAvailable==false)
+    cout<<"ERROR: cannot run away"<<endl;
+  else{
+    runawayAvailable=false;
+    cout<<"You run away"<<endl;
+    player.runaways--;
+    enemigo=createEnemy(enemigo);
+    
+  }
+}
+
+void special(Hero &player,Enemy &enemigo){
+  if(player.special){
+    player.special=false;
+    heroToEnemy(player,enemigo,3);
+    if(enemigo.features.hp>0)
+      enemyToHero(player,enemigo);
+    else{
+      enemigo=createEnemy(enemigo);
+    }
+  }
+  else
+    cout<<"ERROR: special not avaliable"<<endl;
+}
+
+void report(const Hero &player){
+  int totalKills=0;
+  cout<<"[Report]"<<endl;
+  cout<<"Name: "<<player.name<<endl;
+  cout<<"Attack: "<<player.features.attack<<endl;
+  cout<<"Defense: "<<player.features.defense<<endl;
+  cout<<"Health points: "<<player.features.hp<<endl;
+  cout<<"Special: ";
+  if(player.special)
+    cout<<"yes";
+   else
+     cout<<"no";
+   cout<<endl;
+   cout<<"Runaways: "<<player.runaways<<endl;
+   cout<<"Exp: "<<player.exp<<endl;
+   cout<<"Enemies killed:"<<endl;
+   cout<<"- Axolotl: "<<player.kills[0]<<endl;
+   cout<<"- Troll: "<<player.kills[1]<<endl;
+   cout<<"- Orc: "<<player.kills[2]<<endl;
+   cout<<"- Hellhound: "<<player.kills[3]<<endl;
+   cout<<"- Dragon: "<<player.kills[4]<<endl;
+   cout<<"- Total: ";
+   for(int j=0;j<5;j++){
+     totalKills=totalKills+player.kills[j];
+   }
+   cout<<totalKills<<endl;
 }
 
 void showMenu(){
@@ -209,10 +345,8 @@ void showMenu(){
        << "Option: ";
 }
 
-void fight(){
-}
 
-void menu(){
+char menu(Hero &player,Enemy &enemigo,bool &runawayAvailable){
 char op='q';
 do{
   if(op!='1'&&op!='2'&&op!='3'&&op!='4'&&op!='q')
@@ -223,22 +357,28 @@ do{
   
   switch(op){
     case '1':
-    fight();
+    fight(player,enemigo,runawayAvailable);
     break;
     case '2':
+    runaway(player,enemigo,runawayAvailable);
     break;
     case '3':
+    special(player,enemigo);
     break;
     case '4':
+    report(player);
     break;
     case 'q':
     break;
   }
+  return op;
 }
 
 int main(int argc,char *argv[]){
   Hero player;
   Enemy enemigo;
+  char op;
+  bool runawayAvailable=true;
   if(argc!=2){ // Si los parámetros no son correctos, el programa termina inmediatamente
     cout << "Usage: " << argv[0] << " <seed>" << endl;
   }
@@ -249,11 +389,13 @@ int main(int argc,char *argv[]){
     
   player=createHero(player);
   enemigo=createEnemy(enemigo);
-  menu();
-  
-  
-    
+  do{
+  op=menu(player,enemigo,runawayAvailable);
+  }while(player.features.hp>0 && op!='q');
+  if(op!='q'){
+    cout<<"You are dead"<<endl;
+    report(player);	
+  }
     
   }
 }
-
