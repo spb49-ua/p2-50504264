9b107ed3c9a378f91df9cdffa86084391eac8543
spb49-ua <98749056+spb49-ua@users.noreply.github.com>
2022-02-14 13:40:09 +0100

    Mejora de indentado y cambio de nombre de algunas variables

--- game/prac1.cc
+++ game/prac1.cc
@@ -9,6 +9,7 @@ const int KNAME=32;
 const int KENEMIES=5;
 const int KPOINTS=200;
 const int KDICE=20; // Número de caras del dado
+
 const int AXOLOTL_ATTACK=40;
 const int AXOLOTL_DEFENSE=40;
 const int TROLL_ATTACK=60;
@@ -19,6 +20,7 @@ const int HELLHOUND_ATTACK=120;
 const int HELLHOUND_DEFENSE=100;
 const int DRAGON_ATTACK=160;
 const int DRAGON_DEFENSE=140;
+
 const int AXOLOTL_EXP=100;
 const int TROLL_EXP=150;
 const int ORC_EXP=200;
@@ -61,46 +63,40 @@ Hero nombreHero(Hero player){
   int n;
   bool name_error;
   do{
-  name_error=false;
-  cout<<"Enter hero name: ";
-  cin.getline(player.name,KNAME-1);
-  n=strlen(player.name);
-  if(isalpha(player.name[0])==0)
-  	name_error=true;
-  for(int i=0;i<n;i++){
-  	if((isalnum(player.name[i])==0)&&(player.name[i]!=' ')){
-  		name_error=true;
-  	}
-  }
-  if(name_error)
-  	cout<<"ERROR: wrong name"<<endl;
+    name_error=false;
+    cout<<"Enter hero name: ";
+    cin.getline(player.name,KNAME-1);
+    n=strlen(player.name);
+    if(isalpha(player.name[0])==0)
+      name_error=true;
+    for(int i=0;i<n;i++){
+      if(isalnum(player.name[i])==0 && player.name[i]!=' ')
+        name_error=true;
+    }
+    if(name_error)
+      cout<<"ERROR: wrong name"<<endl;
   }while(name_error);
+  
   return player;
 }
 
 Hero attack_defense(Hero player){
   bool error;
-  int n1,n2;
-  char c;
+  int attack,defense;
+  char aux;
+  
   do{
-  error=false;
-  cout<<"Enter attack/defense: ";
-  cin>>n1>>c>>n2;
-  if(n1<=0){
-    cout<<"ERROR: wrong distribution"<<endl;
-    error=true;
-   }
-  if(n2<=0){
-    cout<<"ERROR: wrong distribution"<<endl;
-    error=true;
+    error=false;
+    cout<<"Enter attack/defense: ";
+    cin>>attack>>aux>>defense;
+    if(attack<=0 || defense<=0 || (attack+defense)!=100){
+      cout<<"ERROR: wrong distribution"<<endl;
+      error=true;
     }
-  if((n1+n2)!=100){
-    cout<<"ERROR: wrong distribution"<<endl;
-    error=true;
-   }
   }while(error);
-  player.features.attack=n1*KPOINTS/100;
-  player.features.defense=n2*KPOINTS/100;
+  
+  player.features.attack=attack*KPOINTS/100;
+  player.features.defense=defense*KPOINTS/100;
   player.features.hp=2*player.features.defense;
   
   return player;
@@ -115,6 +111,7 @@ Hero createHero(Hero player){
   for(int i =0;i<KENEMIES;i++){
     player.kills[i]=0;
   }
+  
   return player;
 }
 
@@ -129,30 +126,31 @@ Enemy selectEnemy(Enemy enemigo){
     case 5:
     case 6:
       enemigo.name=AXOLOTL;
-    break;
+      break;
     case 7:
     case 8:
     case 9:
     case 10:
     case 11:
       enemigo.name=TROLL;
-    break;
+      break;
     case 12:
     case 13:
     case 14:
     case 15:
       enemigo.name=ORC;
-    break;
+      break;
     case 16:
     case 17:
     case 18:
       enemigo.name=HELLHOUND;
-    break;
+      break;
     case 19:
     case 20:
       enemigo.name=DRAGON;
-    break;
+      break;
   }
+  
   return enemigo;
 }
 
@@ -165,85 +163,94 @@ Enemy enemyStats(Enemy enemigo){
       enemigo.features.attack=AXOLOTL_ATTACK;
       enemigo.features.defense=AXOLOTL_DEFENSE;
       enemigo.features.hp=2*AXOLOTL_DEFENSE;
-    break;
+      break;
     case 1:
       cout<<"Troll";
       enemigo.features.attack=TROLL_ATTACK;
       enemigo.features.defense=TROLL_DEFENSE;
       enemigo.features.hp=2*TROLL_DEFENSE;
-    break;
+      break;
     case 2:
       cout<<"Orc";
       enemigo.features.attack=ORC_ATTACK;
       enemigo.features.defense=ORC_DEFENSE;
       enemigo.features.hp=2*ORC_DEFENSE;
-    break;
+      break;
     case 3:
       cout<<"Hellhound";
       enemigo.features.attack=HELLHOUND_ATTACK;
       enemigo.features.defense=HELLHOUND_DEFENSE;
       enemigo.features.hp=2*HELLHOUND_DEFENSE;
-    break;
+      break;
     case 4:
-        cout<<"Dragon";
+      cout<<"Dragon";
       enemigo.features.attack=DRAGON_ATTACK;
       enemigo.features.defense=DRAGON_DEFENSE;
-      enemigo.features.	hp=2*DRAGON_DEFENSE;
-    break;
+      enemigo.features.hp=2*DRAGON_DEFENSE;
+      break;
     }
-    cout<<endl;
-    cout<<"Attack: "<<enemigo.features.attack<<endl;
-    cout<<"Defense: "<<enemigo.features.defense<<endl;
-    cout<<"Health points: "<<enemigo.features.hp<<endl;
+    cout<<endl
+        <<"Attack: "<<enemigo.features.attack<<endl
+        <<"Defense: "<<enemigo.features.defense<<endl
+        <<"Health points: "<<enemigo.features.hp<<endl;
+    
     return enemigo;
 }
 
 Enemy createEnemy(Enemy enemigo){
   enemigo=selectEnemy(enemigo);
   enemigo=enemyStats(enemigo);
+  
   return enemigo;
 }
 
 void enemyKilled(Hero &player,Enemy &enemigo){
     player.kills[enemigo.name]++;
+    
     switch(enemigo.name){
       case 0:
-      player.exp=player.exp + AXOLOTL_EXP;
-      break;
+        player.exp=player.exp + AXOLOTL_EXP;
+        break;
       case 1:
-      player.exp=player.exp + TROLL_EXP;
-      break;
+        player.exp=player.exp + TROLL_EXP;
+        break;
       case 2:
-      player.exp=player.exp + ORC_EXP;
-      break;
+        player.exp=player.exp + ORC_EXP;
+        break;
       case 3:
-      player.exp=player.exp + HELLHOUND_EXP;
-      break;
+        player.exp=player.exp + HELLHOUND_EXP;
+        break;
       case 4:
-      player.exp=player.exp + DRAGON_EXP;
-      break;
+        player.exp=player.exp + DRAGON_EXP;
+        break;
     }
 }
 
 void heroToEnemy(Hero &player,Enemy &enemigo,int extra){
-  int n1,n2;
-  n1=rollDice()*5*extra;
-  n2=rollDice()*5;
-  cout<<"[Hero -> Enemy]"<<endl;
-  cout<<"Attack: "<<player.features.attack<<" + "<<n1<<endl;
-  cout<<"Defense: "<<enemigo.features.defense<<" + "<<n2<<endl;
-  cout<<"Hit points: ";
-  if((player.features.attack+n1-(enemigo.features.defense+n2))>0){
-    cout<<player.features.attack+n1-(enemigo.features.defense+n2);
-    enemigo.features.hp=enemigo.features.hp-(player.features.attack+n1-(enemigo.features.defense+n2));
-    }
+  int dice1,dice2;
+  dice1=rollDice()*5*extra;
+  dice2=rollDice()*5;
+  
+  cout<<"[Hero -> Enemy]"<<endl
+      <<"Attack: "<<player.features.attack<<" + "<<dice1<<endl
+      <<"Defense: "<<enemigo.features.defense<<" + "<<dice2<<endl
+      <<"Hit points: ";
+  
+  if((player.features.attack+dice1-(enemigo.features.defense+dice2))>0){
+    cout<<player.features.attack+dice1-(enemigo.features.defense+dice2);
+    enemigo.features.hp=enemigo.features.hp-(player.features.attack+dice1-(enemigo.features.defense+dice2));
+  }
   else
-  	cout<<"0";
-  cout<<endl;
-  cout<<"Enemy health points: ";
+    cout<<"0";
+    
+  cout<<endl
+      <<"Enemy health points: ";
+  
   if(enemigo.features.hp<0)
     enemigo.features.hp=0;
+    
   cout<<enemigo.features.hp<<endl;
+  
   if(enemigo.features.hp==0){
     cout<<"Enemy killed"<<endl;
     enemyKilled(player,enemigo);
@@ -251,23 +258,28 @@ void heroToEnemy(Hero &player,Enemy &enemigo,int extra){
 }
 
 void enemyToHero(Hero &player,Enemy &enemigo){
-  int n1,n2;
-  n1=rollDice()*5;
-  n2=rollDice()*5;
-  cout<<"[Enemy -> Hero]"<<endl;
-  cout<<"Attack: "<<enemigo.features.attack<<" + "<<n1<<endl;
-  cout<<"Defense: "<<player.features.defense<<" + "<<n2<<endl;
-  cout<<"Hit points: ";
-  if((enemigo.features.attack+n1-(player.features.defense+n2))>0){
-    cout<<enemigo.features.attack+n1-(player.features.defense+n2);
-    player.features.hp=player.features.hp-(enemigo.features.attack+n1-(player.features.defense+n2));
-    }
+  int dice1,dice2;
+  dice1=rollDice()*5;
+  dice2=rollDice()*5;
+  
+  cout<<"[Enemy -> Hero]"<<endl
+      <<"Attack: "<<enemigo.features.attack<<" + "<<dice1<<endl
+      <<"Defense: "<<player.features.defense<<" + "<<dice2<<endl
+      <<"Hit points: ";
+  
+  if((enemigo.features.attack+dice1-(player.features.defense+dice2))>0){
+    cout<<enemigo.features.attack+dice1-(player.features.defense+dice2);
+    player.features.hp=player.features.hp-(enemigo.features.attack+dice1-(player.features.defense+dice2));
+  }
   else
   	cout<<"0";
-  cout<<endl;
-  cout<<"Hero health points: ";
+  	
+  cout<<endl
+      <<"Hero health points: ";
+  
   if(player.features.hp<0)
     player.features.hp=0;
+    
   cout<<player.features.hp<<endl;
 }
 
@@ -289,7 +301,6 @@ void runaway(Hero &player,Enemy &enemigo,bool &runawayAvailable){
     cout<<"You run away"<<endl;
     player.runaways--;
     enemigo=createEnemy(enemigo);
-    
   }
 }
 
@@ -297,6 +308,7 @@ void special(Hero &player,Enemy &enemigo){
   if(player.special){
     player.special=false;
     heroToEnemy(player,enemigo,3);
+    
     if(enemigo.features.hp>0)
       enemyToHero(player,enemigo);
     else{
@@ -309,29 +321,34 @@ void special(Hero &player,Enemy &enemigo){
 
 void report(const Hero &player){
   int totalKills=0;
-  cout<<"[Report]"<<endl;
-  cout<<"Name: "<<player.name<<endl;
-  cout<<"Attack: "<<player.features.attack<<endl;
-  cout<<"Defense: "<<player.features.defense<<endl;
-  cout<<"Health points: "<<player.features.hp<<endl;
-  cout<<"Special: ";
+  
+  cout<<"[Report]"<<endl
+      <<"Name: "<<player.name<<endl
+      <<"Attack: "<<player.features.attack<<endl
+      <<"Defense: "<<player.features.defense<<endl
+      <<"Health points: "<<player.features.hp<<endl
+      <<"Special: ";
+  
   if(player.special)
     cout<<"yes";
-   else
-     cout<<"no";
-   cout<<endl;
-   cout<<"Runaways: "<<player.runaways<<endl;
-   cout<<"Exp: "<<player.exp<<endl;
-   cout<<"Enemies killed:"<<endl;
-   cout<<"- Axolotl: "<<player.kills[0]<<endl;
-   cout<<"- Troll: "<<player.kills[1]<<endl;
-   cout<<"- Orc: "<<player.kills[2]<<endl;
-   cout<<"- Hellhound: "<<player.kills[3]<<endl;
-   cout<<"- Dragon: "<<player.kills[4]<<endl;
-   cout<<"- Total: ";
+  else
+    cout<<"no";
+     
+   cout<<endl
+       <<"Runaways: "<<player.runaways<<endl
+       <<"Exp: "<<player.exp<<endl
+       <<"Enemies killed:"<<endl
+       <<"- Axolotl: "<<player.kills[0]<<endl
+       <<"- Troll: "<<player.kills[1]<<endl
+       <<"- Orc: "<<player.kills[2]<<endl
+       <<"- Hellhound: "<<player.kills[3]<<endl
+       <<"- Dragon: "<<player.kills[4]<<endl
+       <<"- Total: ";
+   
    for(int j=0;j<5;j++){
      totalKills=totalKills+player.kills[j];
    }
+   
    cout<<totalKills<<endl;
 }
 
@@ -348,29 +365,29 @@ void showMenu(){
 
 char menu(Hero &player,Enemy &enemigo,bool &runawayAvailable){
 char op='q';
-do{
-  if(op!='1'&&op!='2'&&op!='3'&&op!='4'&&op!='q')
-    cout<<"ERROR: wrong option"<<endl;
-  showMenu();
+
+  do{
+    if(op!='1'&&op!='2'&&op!='3'&&op!='4'&&op!='q')
+      cout<<"ERROR: wrong option"<<endl;
+    showMenu();
   cin>>op;
   }while(op!='1'&&op!='2'&&op!='3'&&op!='4'&&op!='q');
   
   switch(op){
     case '1':
-    fight(player,enemigo,runawayAvailable);
-    break;
+      fight(player,enemigo,runawayAvailable);
+      break;
     case '2':
-    runaway(player,enemigo,runawayAvailable);
-    break;
+      runaway(player,enemigo,runawayAvailable);
+      break;
     case '3':
-    special(player,enemigo);
-    break;
+      special(player,enemigo);
+      break;
     case '4':
-    report(player);
-    break;
-    case 'q':
-    break;
+      report(player);
+      break;
   }
+  
   return op;
 }
 
@@ -379,6 +396,7 @@ int main(int argc,char *argv[]){
   Enemy enemigo;
   char op;
   bool runawayAvailable=true;
+  
   if(argc!=2){ // Si los parámetros no son correctos, el programa termina inmediatamente
     cout << "Usage: " << argv[0] << " <seed>" << endl;
   }
@@ -387,15 +405,15 @@ int main(int argc,char *argv[]){
     
     // Aquí vendrá todo tu código del "main"...
     
-  player=createHero(player);
-  enemigo=createEnemy(enemigo);
-  do{
-  op=menu(player,enemigo,runawayAvailable);
-  }while(player.features.hp>0 && op!='q');
-  if(op!='q'){
-    cout<<"You are dead"<<endl;
-    report(player);	
-  }
-    
+    player=createHero(player);
+    enemigo=createEnemy(enemigo);
+    do{
+      op=menu(player,enemigo,runawayAvailable);
+    }while(player.features.hp>0 && op!='q');
+  
+    if(op!='q'){
+      cout<<"You are dead"<<endl;
+      report(player);	
+    }  
   }
 }
